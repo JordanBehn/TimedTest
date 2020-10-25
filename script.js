3d4650b6a2a9436f76b9c2767dd43f8ef773bf07
JQB <jordanqbehn@gmail.com>
2020-10-25 08:54:52 -0500

    updated html with log of past round fields

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var numCorrect = document.querySelector("#num-correct");
 var minutesLeft = document.querySelector("#minutes");
 var secondsLeft = document.querySelector("#seconds");
 var startButton = document.querySelector("#start-button");
+var quizdiv = document.querySelector(".quiz");
+var savedInitials = document.querySelector("#saved-initials");
+var savedScore = document.querySelector("#saved-score");
+
 var secondsElapsed = 0;
-var totalSeconds = 10;
-var score = 5;
+var totalSeconds = 5;
+var score = 0;
 var questions = [{
         q: "Inside which HTML element do we put the JavaScript?",
         answers: ["<script>", "<code>", "<javascript"],
@@ -32,13 +36,22 @@ var questions = [{
         correctIndex: 2
     },
 ]
-console.log(questions)
-console.log(questions[2].answers[1])
 
-renderTime()
-renderQuestion();
-startTimer()
+//render last round score and initials
+savedInitials.textContent = localStorage.getItem("initials")
+savedScore.textContent = localStorage.getItem("score")
 
+//on start button click, start the time, render the quiz, remove start button
+startButton.addEventListener("click", function() {
+    interval = setInterval(function() {
+        secondsElapsed++;
+        renderTime();
+    }, 1000);
+    startButton.remove();
+    renderQuiz();
+})
+
+//formats remaining minutes
 function getFormattedMinutes() {
     //
     var secondsLeft = totalSeconds - secondsElapsed;
@@ -55,7 +68,7 @@ function getFormattedMinutes() {
 
     return formattedMinutes;
 }
-
+//formats remaining seconds
 function getFormattedSeconds() {
     var secondsLeft = (totalSeconds - secondsElapsed) % 60;
 
@@ -69,24 +82,27 @@ function getFormattedSeconds() {
 
     return formattedSeconds;
 }
-
+//Updates every second, displays the time remaining. At zero time remaining ends quiz
 function renderTime() {
-
     minutesLeft.textContent = getFormattedMinutes();
     secondsLeft.textContent = getFormattedSeconds();
     if (secondsElapsed >= totalSeconds) {
-        clearInterval(interval);
-        numCorrect.textContent = "Final Score: " + score;
-        //resets timer and goes to score page when time is up
+        endQuiz();
     }
 }
 
-function startTimer() {
-    interval = setInterval(function() {
-        secondsElapsed++;
-        renderTime();
-    }, 1000);
+function renderQuiz() {};
+//ends timer, saves initials and score, creates button to restart quiz
+function endQuiz() {
+    clearInterval(interval);
+    numCorrect.textContent = "Final Score: " + score;
+    questiondiv.remove();
+    var initials = prompt("Nice score! Enter your intials?")
+    localStorage.setItem("initials", initials)
+    localStorage.setItem("score", score)
+
+    var restartBtn = document.createElement("button");
+    restartBtn.innerHTML = "Try again?";
+    quizdiv.appendChild(restartBtn);
+    restartBtn.addEventListener("click", function() { location.reload() })
 }
-
-
-function renderQuestion() {};
